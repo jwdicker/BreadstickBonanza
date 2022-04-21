7ebfc22567f46802c49658ad6bffd5fb21b4a74f
Jacob Dickerman <jwdicker@ucsc.edu>
2022-04-20 19:23:30 -0700

    Added canvas

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 console.log("Foo");
 console.log("Bar");
+
+// Names:
+// Alexis Adolpho
+// Jacob Dickerman
+// Grce Ho
+// 
+// Sources: 
+// https://phaser.discourse.group/t/center-game-on-my-website/5921/3
+
+const config = {
+    type: Phaser.CANVAS,
+    width: 540,
+    height: 960,
+    autoCenter: true,
+    scene: [LoadScene]
+};
+
+let game = new Phaser.Game(config);
---
+++ src/scenes/load.js
@@ -0,0 +1,10 @@
+class LoadScene extends Phaser.Scene {
+    
+    constructor() {
+        super("load");
+    }
+
+    create() {
+        console.log("Loaded");
+    }
+}
