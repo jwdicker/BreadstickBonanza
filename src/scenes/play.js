802bfc422cb961c8cd05ff172f736216ae7ace7c
Jacob Dickerman <jwdicker@ucsc.edu>
2022-04-23 16:34:47 -0700

    Added game over text

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -46,7 +46,22 @@ class Play extends Phaser.Scene {
     // Handles what happens when the player hits a meatball
     gameEnd() {
         this.physics.world.pause();
-        console.log("game over");
+
+        // Text setup
+        let textConfig = {
+            fontFamily: "Helvetica",
+            fontsize: "36 px",
+            backgroundColor: "#FFF",
+            color: "#000",
+            align: "center",
+            padding: {
+                top: 5,
+                bottom: 5
+            }
+        };
+        this.add.text(game.config.width / 2, game.config.height / 2 - 32, "GAME OVER", textConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2 + 32, "Press ↓ to restart", textConfig).setOrigin(0.5);
+
         this.endOGame = true;
     }
 
